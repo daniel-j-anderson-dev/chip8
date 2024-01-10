2812f4394c36ec2cf301fd46e3c01d47699adea2
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-01-09 22:30:05 -0500

    Updated error display

--- src/chip8/error.rs
+++ src/chip8/error.rs
@@ -4,9 +4,13 @@ pub enum Chip8Error {
 }
 impl std::fmt::Display for Chip8Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Chip8 Error: {}", match self {
-            Chip8Error::ProgramTooLarge => String::from(""),
-        })
+        write!(
+            f,
+            "Chip8 Error: {}",
+            match self {
+                Chip8Error::ProgramTooLarge => String::from(""),
+            }
+        )
     }
 }
 impl std::error::Error for Chip8Error {}
