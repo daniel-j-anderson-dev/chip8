0f6c6f62560eacbdf4cdd61b5addaa0bf7b94274
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-08-25 15:15:38 -0600

    made initializer

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,9 @@ pub struct Chip8 {
     /// The last byte (`VF`) is also used as a flag for carries or other purposes
     variable_register: [u8; 16],
 
-    /// `false` represents a black pixel. `true` represents a white pixel
-    display: [[bool; 64]; 32],
-
     /// Keeps track of return memory locations when a subroutine is called
     call_stack: [u16; 16],
-    
+
     /// Keeps track of which entry in the stack should be returned to.
     call_stack_index: usize,
 
@@ -29,8 +26,25 @@ pub struct Chip8 {
 
     /// Decrements at 60hz until zero when a sound is played
     sound_timer: u8,
+
+    /// `false` represents a black pixel. `true` represents a white pixel
+    display: [[bool; 64]; 32],
 }
 
 impl Chip8 {
-    pub const PROGRAM_MEMORY_OFFSET: usize = 200;
+    pub const PROGRAM_MEMORY_OFFSET: u16 = 200;
+
+    pub fn new() -> Chip8 {
+        Self {
+            memory: [0; 4096],
+            program_counter: Self::PROGRAM_MEMORY_OFFSET,
+            address_register: 0,
+            variable_register: [0; 16],
+            call_stack: [0; 16],
+            call_stack_index: 0,
+            delay_timer: 0,
+            sound_timer: 0,
+            display: [[false; 64]; 32],
+        }
+    }
 }
