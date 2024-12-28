97fcb6ce85f45248fc3c79c57f46289d4bf15bbc
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-12-28 14:30:13 -0700

    implemented instructions for IBM logo rom

--- src/interpreter/instructions.rs
+++ src/interpreter/instructions.rs
@@ -1,13 +1,12 @@
-use crate::interpreter::Chip8;
+use crate::interpreter::Interpreter;
 
-impl Chip8 {
+impl Interpreter {
     pub(super) fn clear_screen(&mut self) {
         self.display = Self::BLACK_DISPLAY;
     }
 
     pub(super) fn return_subroutine(&mut self) {
-        self.call_stack_index -= 1;
-        self.program_counter = self.call_stack[self.call_stack_index as usize];
+        unimplemented!();
     }
 
     pub(super) fn jump(&mut self, address: u16) {
@@ -15,48 +14,35 @@ impl Chip8 {
     }
 
     pub(super) fn call_subroutine(&mut self, address: u16) {
-        let old_program_counter = self.program_counter as usize;
-        self.program_counter += 1;
-
-        self.call_stack[old_program_counter] = self.program_counter;
-
-        self.program_counter = address;
+        unimplemented!();
     }
 
     /// Skips the next instruction if
     /// `variable_register[x_register_index]` is equal to last byte of the instruction
     pub(super) fn skip_if_equal_value(&mut self, x_register_index: usize, value: u8) {
-        if self.variable_register[x_register_index] == value {
-            self.program_counter += 2;
-        }
+        unimplemented!();
     }
 
     /// Skips the next instruction if
     /// `variable_register[x_register_index]` is NOT equal to last byte of the instruction
     pub(super) fn skip_if_not_equal_value(&mut self, x_register_index: usize, value: u8) {
-        // if (registers[(opcode & 0x0F00u) >> 8u] != (opcode & 0x00FFu)) pc += 2;
-        if self.variable_register[x_register_index] != value {
-            self.program_counter += 2;
-        }
+        unimplemented!();
     }
 
     /// Skips the next instruction if
     /// `variable_register[x_register_index]` equals `variable_register[y_register_index]`
     pub(super) fn skip_if_equal(&mut self, x_register_index: usize, y_register_index: usize) {
-        // if (registers[(opcode & 0x0F00u) >> 8u] == registers[(opcode & 0x00F0u) >> 4u]) pc += 2;
-        if self.variable_register[x_register_index] == self.variable_register[y_register_index] {
-            self.program_counter == 2;
-        }
+        unimplemented!();
     }
 
     /// Sets `variable_register[x_register_index]` to value
     pub(super) fn assign_value(&mut self, x_register_index: usize, value: u8) {
-        unimplemented!();
+        self.variable_register[x_register_index] = value;
     }
 
     /// Adds value to `variable_register[x_register_index]` (carry flag is not changed)
     pub(super) fn add_assign_value(&mut self, x_register_index: usize, value: u8) {
-        unimplemented!();
+        self.variable_register[x_register_index] += value;
     }
 
     /// Sets `variable_register[x_register_index]` to the value of `variable_register[y_register_index]`
@@ -65,17 +51,17 @@ impl Chip8 {
     }
 
     /// Sets `variable_register[x_register_index]` to (`variable_register[x_register_index]` or `variable_register[y_register_index]`) bitwise
-    pub(super) fn bitwise_or(&mut self, x_register_index: usize, y_register_index: usize) {
+    pub(super) fn bitwise_or_assign(&mut self, x_register_index: usize, y_register_index: usize) {
         unimplemented!();
     }
 
     /// Sets `variable_register[x_register_index]` to `variable_register[x_register_index]` and `variable_register[y_register_index]` (bitwise)
-    pub(super) fn bitwise_and(&mut self, x_register_index: usize, y_register_index: usize) {
+    pub(super) fn bitwise_and_assign(&mut self, x_register_index: usize, y_register_index: usize) {
         unimplemented!();
     }
 
     /// Sets `variable_register[x_register_index]` to `variable_register[x_register_index]` xor `variable_register[y_register_index]` (bitwise)
-    pub(super) fn bitwise_xor(&mut self, x_register_index: usize, y_register_index: usize) {
+    pub(super) fn bitwise_xor_assign(&mut self, x_register_index: usize, y_register_index: usize) {
         unimplemented!();
     }
 
@@ -110,8 +96,8 @@ impl Chip8 {
     }
 
     /// Sets address_register to the address address
-    pub(super) fn set_address_register(&mut self, address: u16) {
-        unimplemented!();
+    pub(super) fn address_register_assign(&mut self, address: u16) {
+        self.address_register = address
     }
 
     /// Jumps to the address address plus variable_register[0]
@@ -124,8 +110,40 @@ impl Chip8 {
         unimplemented!();
     }
 
-    pub(super) fn draw_sprite(&mut self, x_register_index: usize, y_register_index: usize, sprite_height: u8) {
-        unimplemented!();
+    pub(super) fn draw_sprite(
+        &mut self,
+        x_register_index: usize,
+        y_register_index: usize,
+        sprite_height: u8,
+    ) {
+        let address_register = self.address_register as usize;
+        let x_position = self.variable_register[x_register_index] as usize % Self::DISPLAY_WIDTH;
+        let y_position = self.variable_register[y_register_index] as usize % Self::DISPLAY_HEIGHT;
+
+        self.variable_register[0xF] = 0;
+
+        for sprite_row_index in 0..sprite_height as usize {
+            let display_row_index = y_position + sprite_row_index;
+            
+            let sprite_byte = self.memory[address_register + sprite_row_index];
+
+            for sprite_column_index in 0..8 {
+                let display_column_index = x_position + sprite_column_index;
+
+                let pixel_bitmask = 0b10000000 >> sprite_column_index as u8;
+                let sprite_pixel = (sprite_byte & pixel_bitmask) > 0;
+
+                let screen_pixel = &mut self.display[display_row_index][display_column_index];
+
+                if sprite_pixel {
+                    if *screen_pixel {
+                        self.variable_register[0xF] = 1;
+                    }
+
+                    *screen_pixel ^= true;
+                }
+            }
+        }
     }
 
     /// Skips the next instruction if the key stored in `variable_register[x_register_index]` is pressed
@@ -149,12 +167,12 @@ impl Chip8 {
     }
 
     /// Sets the delay timer to `variable_register[x_register_index]`
-    pub(super) fn set_delay_timer(&mut self, x_register_index: usize) {
+    pub(super) fn delay_timer_assign(&mut self, x_register_index: usize) {
         unimplemented!();
     }
 
     /// Sets the sound timer to `variable_register[x_register_index]`
-    pub(super) fn set_sound_timer(&mut self, x_register_index: usize) {
+    pub(super) fn sound_timer_assign(&mut self, x_register_index: usize) {
         unimplemented!();
     }
 
@@ -165,12 +183,12 @@ impl Chip8 {
 
     /// Sets address_register to the location of the sprite for the character in `variable_register[x_register_index]`
     /// Font starts at memory address 0
-    pub(super) fn set_address_register_to_character_address(&mut self, x_register_index: usize) {
+    pub(super) fn address_register_assign_character_address(&mut self, x_register_index: usize) {
         unimplemented!();
     }
 
     /// Stores the binary-coded decimal representation of variable_register[x_register_index], with the hundreds digit in memory at location in address_register, the tens digit at location address_register+1, and the ones digit at location address_register+2
-    pub(super) fn store_binary_coded_decimal_at_address_register(&mut self, x_register_index: usize) {
+    pub(super) fn store_binary_coded_decimal_address(&mut self, x_register_index: usize) {
         unimplemented!();
     }
 
