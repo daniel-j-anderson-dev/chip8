b8349dd10870192c2dbf5f817ea5908aa9deeea8
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-01-14 17:44:15 -0700

    implemented Getters macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
-use syn::Ident;
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro]
 pub fn generate_terminal_tests(_: TokenStream) -> TokenStream {
@@ -34,4 +35,81 @@ pub fn generate_terminal_tests(_: TokenStream) -> TokenStream {
         .collect::<Result<Vec<_>, Box<dyn ::core::error::Error>>>()
         .unwrap();
     quote! { #(#tests)* }.into()
+}
+
+/// Derives getter methods for each field using the field name. Each getter will return a reference to the field
+#[proc_macro_derive(Getters)]
+pub fn generate_getters(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let struct_identifier = &input.ident;
+    let fields = if let syn::Data::Struct(data) = &input.data {
+        &data.fields
+    } else {
+        panic!("Getters can only be derived on structs");
+    };
+
+    let getters = fields.iter().map(|field| {
+        let field_identifier = &field.ident;
+        let field_type = &field.ty;
+
+        quote! {
+            pub fn #field_identifier(&self) -> &#field_type {
+                &self.#field_identifier
+            }
+        }
+    });
+
+    quote! {
+        impl #struct_identifier {
+            #(#getters)*
+        }
+    }
+    .into()
+}
+
+/// Derives getter methods for each field using the field name. This can only be derived for structs that derive [Copy]
+#[proc_macro_derive(CopyGetters)]
+pub fn generate_copy_getters(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let does_struct_derive_copy = input.attrs.iter().any(|attribute| {
+        attribute.path().is_ident("derive")
+            && attribute
+                .parse_nested_meta(|metadata| {
+                    metadata.path.is_ident("Copy").then_some(()).ok_or_else(|| {
+                        syn::Error::new(
+                            proc_macro2::Span::call_site(),
+                            "CopyGetters can only be derived on types that derive Copy",
+                        )
+                    })
+                })
+                .is_ok()
+    });
+    if does_struct_derive_copy {
+        panic!("CopyGetters can only be derived on types that derive Copy")
+    };
+
+    let struct_identifier = &input.ident;
+    let fields = if let syn::Data::Struct(data) = &input.data {
+        &data.fields
+    } else {
+        panic!("CopyGetters can only be derived on structs");
+    };
+
+    let getters = fields.iter().map(|field| {
+        let field_identifier = &field.ident;
+        let field_type = &field.ty;
+
+        quote! {
+            pub fn #field_identifier(&self) -> #field_type {
+                self.#field_identifier
+            }
+        }
+    });
+
+    quote! {
+        impl #struct_identifier {
+            #(#getters)*
+        }
+    }
+    .into()
 }
